06556b6527f160089942ba9d5ac67d18a5ca3108
ShanaGari <shanagari7@gmail.com>
2025-02-25 19:26:33 +0530

    added a while loop to take the number of players

--- pig.py
+++ pig.py
@@ -6,5 +6,15 @@ def roll():
     roll= random.randint(min_value,max_value)
     return roll
 
-value=roll()
-print(value)
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players=int(players)
+        if 2<=players<=4:
+            break
+        else:
+            print("Invalid input. Please enter a number between 2 and 4.")
+    else:
+        print("Invalid input. Please enter a number between 2 and 4.")
+
+print(players)
